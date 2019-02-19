eee95f12d7772ef7c1505f51c87746a31124a011
ll <1875115505@qq.com>
2019-02-19 20:03:28 +0800

    第三版

--- update01/src/com/itheima/git/SecondClass.java
+++ update01/src/com/itheima/git/SecondClass.java
@@ -1,22 +1,36 @@
 package com.itheima.git;
 
+import org.junit.Test;
+
 public class SecondClass {
 
-
-        public  static void main(){
-           SecondClass fs=new SecondClass();
-            int result= fs.add(5);
-            System.out.println(result);
-        }
-        public int add(int a ){
-            int sum=0;
-            for (int i = 0; i < a; i++) {
-                sum+=i;
-            }
-
-            return sum;
-
-        }
+@Test
+    public void main() {
+        SecondClass fs = new SecondClass();
+        int result = fs.add(5);
+        System.out.println(result);
     }
+
+    public  static int add(int a) {
+        int sum = 0;
+        for (int i = 0; i < a; i++) {
+            sum += i;
+        }
+
+        return sum;
+
+    }
+
+    public int add1(int a) {
+        int sum = 0;
+        for (int i = 0; i < a; i++) {
+            sum += i;
+        }
+
+        return sum;
+
+
+    }
+}
 
 
